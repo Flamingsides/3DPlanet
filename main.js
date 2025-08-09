73117c0ac09f3f6efacca98f4a63bd9556bc5eff
Zuberi, Suhaib <sz2056@hw.ac.uk>
2025-08-09 19:08:37 +0800

    Improved Modularity Used functions to make code more modular

--- main.js
+++ main.js
@@ -3,13 +3,70 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+/** Globals **/
+const radius = 30; // Define the radius of the circular path
+const numPoints = 100; // Number of points for the path
+var points = {};
+
 // Ensure canvas readjusts to window size
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
+document.addEventListener('keydown', event => {
+    console.log(event);
+    keyDownHandler(event.key);
+});
+
+function keyDownHandler(key) {
+    if (!camera)
+        console.log('Camera object not initialised!');
+
+    switch (key) {
+        case 'w':
+            camera.position.y += 1;
+            // model.rotation.x += 0.01;
+            break;
+        case 's':
+            camera.position.y -= 1;
+            // model.rotation.x -= 0.01;
+            break;
+        case 'd':
+            camera.position.x += 1;
+            // model.rotation.y += 0.01;
+            break;
+        case 'a':
+            camera.position.x -= 1;
+            // model.rotation.y -= 0.01;
+            break;
+        case 'e':
+            camera.position.z += 1;
+            // model.rotation.z += 0.01;
+            break;
+        case 'q':
+            camera.position.z -= 1;
+            // model.rotation.z -= 0.01;
+            break;
+        default:
+            console.log('Non-specific Key Press');
+            break;
+    }
+    console.log(camera.position);
+}
+
+function getModelDimensions(model) {
+    // Compute the bounding box to determine its height
+    const box = new THREE.Box3().setFromObject(model);
+    const dimensions = new THREE.Vector3();
+    box.getSize(dimensions);
+
+    return dimensions;
+}
+
 // Initialize scene, camera, and renderer
 const scene = new THREE.Scene();
+scene.background = new THREE.Color().setHex(0x87CEEB);
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
@@ -42,14 +99,12 @@ loader.load(
         // Scale the model
         model.scale.set(5, 5, 5);
 
-        // Compute the bounding box to determine its height
-        const box = new THREE.Box3().setFromObject(model);
-        const size = new THREE.Vector3();
-        box.getSize(size);
+        const dimensions = getModelDimensions(model);
 
         // Set position to be below the ground
-        const belowGround = -size.y / 2 - 1; // Subtract 1 or adjust as necessary
+        const belowGround = -dimensions.y / 2 - 1; // Subtract 1 or adjust as necessary
         model.position.set(0, belowGround, 0); // Adjust the Y position to below the ground
+
 
         scene.add(model);
         console.log('Model loaded successfully');
@@ -70,6 +125,10 @@ loader.load(
         // Adjust scale based on your scene dimensions
         rocket.scale.set(3, 3, 3); // Scale the rocket model
 
+        points = genRocketPathPoints(radius, numPoints);
+        const pathObject = createPathFromPoints(points, 0xFF0000);
+        scene.add(pathObject);
+
         scene.add(rocket);
     },
     undefined,
@@ -85,49 +144,30 @@ scene.add(gridHelper);
 // mouse controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// // Define the path using CatmullRomCurve3 for a smooth curve
-// const points = new THREE.CatmullRomCurve3([
-//   new THREE.Vector3(10, 0, 10),
-//   new THREE.Vector3(10, 1, 10),
-// ]);
+function genRocketPathPoints(radius, numPoints) {
+    // Create a circular path on the x-z plane, with gradual downward movement on the y-axis
+    const points = [];
+    const z = 0; // y value is unchanging
+    const rocketYOffset = getModelDimensions(rocket).y / 2;
+    var xStretch = 1.5, zStretch = 4;
+    for (let i = 0; i <= numPoints; i++) {
+        const angle = (i / numPoints) * Math.PI * 2; // Angle for the circle
+        const x = -radius * (Math.cos(angle) - 1) * xStretch; // X coordinate of the point on the circle (left-right)
+        const y = radius * Math.sin(angle) * zStretch + rocketYOffset;; // Y coordinate of the point on the circle (up-down)
+        points.push(new THREE.Vector3(x, y, z)); // Store each point on the path
+    }
 
-// const pathGeometry = new THREE.BufferGeometry().setFromPoints(
-//   points.getPoints(50)
-// );
-// const pathMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
-// const pathObject = new THREE.Line(pathGeometry, pathMaterial);
-// scene.add(pathObject);
-
-const radius = 30; // Define the radius of the circular path
-const numPoints = 100; // Number of points for the path
-
-// Create a circular path on the x-z plane, with gradual downward movement on the y-axis
-const points = [];
-const y = 0; // y value is unchanging
-for (let i = 0; i <= numPoints; i++) {
-    const angle = (i / numPoints) * Math.PI * 2; // Angle for the circle
-    const x = radius * (Math.cos(angle) - 1) * 1.5; // X coordinate of the point on the circle (left-right)
-    const z = radius * Math.sin(angle) * 4; // Z coordinate of the point on the circle (up-down)
-    points.push(new THREE.Vector3(x, y, z)); // Store each point on the path
+    return points;
 }
 
-// Apply rotation directly to the path points
-const rotationMatrix = new THREE.Matrix4(); // Create a matrix to rotate the path
+function createPathFromPoints(points, color) {
+    // Create the curve from the rotated points
+    const pathGeometry = new THREE.BufferGeometry().setFromPoints(points);
+    const pathMaterial = new THREE.LineBasicMaterial({ color: color });
+    const pathObject = new THREE.Line(pathGeometry, pathMaterial);
 
-// Rotating the path by 45 degrees on the x-axis and y-axis
-rotationMatrix.makeRotationX(Math.PI / 2); // Rotate around the X-axis
-rotationMatrix.multiply(new THREE.Matrix4().makeRotationY(Math.PI)); // Rotate around the Y-axis
-
-// Apply the rotation to each point in the path
-points.forEach(point => point.applyMatrix4(rotationMatrix));
-
-// Create the curve from the rotated points
-const pathGeometry = new THREE.BufferGeometry().setFromPoints(points);
-const pathMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
-const pathObject = new THREE.Line(pathGeometry, pathMaterial);
-
-// Add the path to the scene
-scene.add(pathObject);
+    return pathObject;
+}
 
 // Function to calculate it based on scroll position
 function getScrollT() {
@@ -141,6 +181,7 @@ function getScrollT() {
 }
 
 const zoomSpeed = 0.0007; // Define the zoom out speed
+camera.position.set(0, -1, 35);
 
 // Animation loop
 function animate() {
@@ -150,23 +191,12 @@ function animate() {
     if (!model)
         return
 
-    model.rotation.y += 0.001; // Step 4: Rotate the model if it's loaded
-    model.rotation.x += 0.0001;
-    model.rotation.z += 0.0001;
+    // model.rotation.y += 0.001; // Step 4: Rotate the model if it's loaded
+    // model.rotation.x += 0.0001;
+    // model.rotation.z += 0.0001;
 
     const t = getScrollT(); // Get t based on scroll position
-    // console.log(t);
     if (rocket) {
-        // const position = points.getPointAt(t); // Position along the path
-        // rocket.position.copy(position); // Update rocket's position
-
-        // // Make the rocket face the path direction
-        // // Get tangent for rotation
-        // const tangent = points.getTangentAt(t).normalize(); 
-        // // gives the rocket a forward-looking target based on the path’s direction at that point
-        // rocket.lookAt(position.clone().add(tangent));
-
-
         // Find the rocket's position along the path based on scroll percentage 't'
         const positionIndex = Math.floor(t * numPoints);
         const position = points[positionIndex]; // Get the position at 't'
@@ -181,9 +211,10 @@ function animate() {
 
     // Zoom out effect based on scroll
     const zoomFactor = Math.max(1, 1 + (scrollY * zoomSpeed)); // Ensure it doesn't zoom in
-    camera.position.set(0, 1 * zoomFactor, 35 * zoomFactor); // Adjust the camera position
+    // camera.position.set(0, 1 * zoomFactor, 35 * zoomFactor); // Adjust the camera position
     //camera.position.set(0, 1, 35); 
 
+    camera.lookAt(camera.position.clone().setZ(0))
     controls.update();
     renderer.render(scene, camera);
 
